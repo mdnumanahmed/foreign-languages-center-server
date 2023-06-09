79cda6a1a47710388e6d60689ee301170a18ab55
mdnumanahmed <mnumansyl@gmail.com>
2023-06-10 03:19:31 +0600

    added post api for users

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 // const uri = "mongodb://localhost:27017"
 
@@ -20,21 +18,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const userCollection = client.db('flc_db').collection('users')
+    const userCollection = client.db("flc_db").collection("users");
 
-
-
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const registeredUser = await userCollection.findOne(query);
+      if (registeredUser) {
+        return res.send({ message: "User already registered" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -42,11 +50,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("FLC is running");
+});
 
-app.get('/', (req, res) => {
-    res.send('FLC is running')
-})
-
-app.listen(port, ()=> {
-    console.log(`FLC server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`FLC server is running on port: ${port}`);
+});
