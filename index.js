fe3dca15f36672f0fb57313969a5d34144b76cff
mdnumanahmed <mnumansyl@gmail.com>
2023-06-11 19:49:33 +0600

    added api for checking admin and verify admin middleware

--- index.js
+++ index.js
@@ -56,6 +56,18 @@ async function run() {
       res.send({ token });
     });
 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
