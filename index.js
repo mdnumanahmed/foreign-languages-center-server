409f8b3048562f40cee915f968f6e990361252f4
mdnumanahmed <mnumansyl@gmail.com>
2023-06-11 19:49:19 +0600

    added api for checking admin and verify admin middleware

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  //bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // const uri = "mongodb://localhost:27017"
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@flc.panjdap.mongodb.net/?retryWrites=true&w=majority`;
@@ -44,6 +64,17 @@ async function run() {
         return res.send({ message: "User already registered" });
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
       res.send(result);
     });
 
